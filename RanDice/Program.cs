ead3ccc2a8c8d985550851d84f583283b29bbc28
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-20 20:46:39 +0000

    Parse cm line args, throw n dice, sum them and display result

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,7 +6,17 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+            Random random = new Random(s);
+
+            int sum=0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += random.Next(1,7);
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
