a82eb324fb5d6f7bc990535d6d1bda5529d0a180
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-20 23:41:28 +0000

    PickupPowerUp and TakeDamage test

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -15,6 +15,14 @@ namespace MyGame
                 enemies[i] = new Enemy(Console.ReadLine());
             }
 
+            Console.WriteLine($"{enemies[0].GetName()} {enemies[0].GetHealth()} {enemies[0].GetShield()}");
+            enemies[0].PickupPowerUp(PowerUp.Shield, 200);
+            Console.WriteLine($"{enemies[0].GetName()} {enemies[0].GetHealth()} {enemies[0].GetShield()}");
+            enemies[0].TakeDamage(150);
+            Console.WriteLine($"{enemies[0].GetName()} {enemies[0].GetHealth()} {enemies[0].GetShield()}");
+            enemies[0].TakeDamage(100);
+            enemies[0].PickupPowerUp(PowerUp.Health,100);
+
             for (int i = 0; i < n; i++)
             {
                 string name = enemies[i].GetName();
