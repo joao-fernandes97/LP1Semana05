e037643d4b9f9a34ecb4f8eadcc2809728048148
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-20 22:26:51 +0000

    instantiates Enemy objects with read names in the corresponding array pos

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -8,6 +8,12 @@ namespace MyGame
         {
             int n = int.Parse(args[0]);
             Enemy[] enemies = new Enemy[n];
-        }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Nome do inimigo {i+1}: ");
+                enemies[i] = new Enemy(Console.ReadLine());
+            }
+        }        
     }
 }
