be42f1a0eff9fdc7b7f1e6a87c105c00ea43c1f1
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-20 20:09:35 +0000

    Convert msec to sec and display with 3 decimal places

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -16,6 +16,10 @@ namespace Chronos
             Thread.Sleep(200);
             crono1.Stop();
             crono2.Stop();
+            float elapsedSeconds1 = (float)crono1.ElapsedMilliseconds/10000;
+            float elapsedSeconds2 = (float)crono2.ElapsedMilliseconds/1000;
+            Console.WriteLine(elapsedSeconds1.ToString("0.000"));
+            Console.WriteLine(elapsedSeconds2.ToString("0.000"));
         }
     }
 }
