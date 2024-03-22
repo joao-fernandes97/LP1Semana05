27256eeb60c5de040dc71fbeb3062f2ed68988fd
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-22 16:29:34 +0000

    SetEnemyInRoom method

--- JogoDeSexta/Level.cs
+++ JogoDeSexta/Level.cs
@@ -14,5 +14,10 @@ namespace JogoDeSexta
            this.difficulty = difficulty
            enemies = new Enemy[rooms];
         }
+
+        public void SetEnemyInRoom(int room, Enemy enemy)
+        {
+            enemies[room] = enemy;
+        }
     }
 }
